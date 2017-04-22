31ae08c7a53763f40d3187c49a24e6bca761929d
Naeem Akram Malik <naeem.akram.malik@gmail.com>
2017-04-22 17:49:53 +0500

    Console application up to Socket.Accept method call done and demostrated

---
+++ SocketsServerStarter/SocketsServerStarter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketsServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            listenerSocket.Bind(ipep);
+
+            listenerSocket.Listen(5);
+            listenerSocket.Accept();
+
+
+        }
+    }
+}
