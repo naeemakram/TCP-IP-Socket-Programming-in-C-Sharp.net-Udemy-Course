4034d5f8cb6bc17ff6ae25612e53d005ceb606e0
Naeem Akram Malik <naeem.akram.malik@gmail.com>
2017-04-22 21:22:43 +0500

    04 using encoding to covert bytes to string.

--- SocketsServerStarter/SocketsServerStarter/Program.cs
+++ SocketsServerStarter/SocketsServerStarter/Program.cs
@@ -22,7 +22,28 @@ namespace SocketsServerStarter
             listenerSocket.Bind(ipep);
 
             listenerSocket.Listen(5);
-            listenerSocket.Accept();
+
+            Console.WriteLine("About to accept incoming connection.");
+
+            Socket client = listenerSocket.Accept();
+
+            Console.WriteLine("Client connected. " + client.ToString() + " - IP End Point: " + client.RemoteEndPoint.ToString());
+
+            byte[] buff = new byte[128];
+
+            int numberOfReceivedBytes = 0;
+
+            numberOfReceivedBytes = client.Receive(buff);
+
+            Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by client is: " + buff);
+
+            string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by client is: " + receivedText);
+
+            
 
 
         }
