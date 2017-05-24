6242fb51978979dcc32ed976d691e95fd3a294c9
Naeem Akram Malik <naeem.akram.malik@gmail.com>
2017-05-24 09:32:36 +0500

    18 - Reading data sent from the client on network stream with StreamReader

--- UdemyAsyncSocketServer/LahoreSocketAsync/LahoreSocketServer.cs
+++ UdemyAsyncSocketServer/LahoreSocketAsync/LahoreSocketServer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -14,6 +16,7 @@ namespace LahoreSocketAsync
         int mPort;
         TcpListener mTCPListener;
 
+        public bool KeepRunning { get; set; }
 
         public async void StartListeningForIncomingConnection(IPAddress ipaddr = null, int port = 23000)
         {
@@ -33,11 +36,69 @@ namespace LahoreSocketAsync
             System.Diagnostics.Debug.WriteLine(string.Format("IP Address: {0} - Port: {1}", mIP.ToString(), mPort));
 
             mTCPListener = new TcpListener(mIP, mPort);
-            mTCPListener.Start();
 
-            var returnedByAccept = await mTCPListener.AcceptTcpClientAsync();
+            try
+            {
+                mTCPListener.Start();
 
-            System.Diagnostics.Debug.WriteLine("Client connected successfully: " + returnedByAccept.ToString());
+                KeepRunning = true;
+
+                while (KeepRunning)
+                {
+                    var returnedByAccept = await mTCPListener.AcceptTcpClientAsync();
+
+                    System.Diagnostics.Debug.WriteLine("Client connected successfully: " + returnedByAccept.ToString());
+
+                    TakeCareOfTCPClient(returnedByAccept);
+                }
+
+            }
+            catch (Exception excp)
+            {
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
+        }
+
+        private async void TakeCareOfTCPClient(TcpClient paramClient)
+        {
+            NetworkStream stream = null;
+            StreamReader reader = null;
+
+            try
+            {
+                stream = paramClient.GetStream();
+                reader = new StreamReader(stream);
+
+                char[] buff = new char[64];
+
+                while (KeepRunning)
+                {
+                    Debug.WriteLine("*** Ready to read");
+
+                    int nRet = await reader.ReadAsync(buff, 0, buff.Length);
+
+                    System.Diagnostics.Debug.WriteLine("Returned: " + nRet);
+
+                    if (nRet == 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Socket disconnected");
+                        break;
+                    }
+
+                    string receivedText = new string(buff);
+
+                    System.Diagnostics.Debug.WriteLine("*** RECEIVED: " + receivedText);
+
+                    Array.Clear(buff, 0, buff.Length);
+
+
+                }
+
+            }
+            catch (Exception excp)
+            {
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
 
         }
     }
