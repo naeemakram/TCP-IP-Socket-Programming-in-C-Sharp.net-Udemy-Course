ca41e8f82a103de7d64f8d62010d985a9ef002b2
Naeem Akram Malik <naeem.akram.malik@gmail.com>
2017-06-08 00:19:49 +0500

    after sendtoallclients demo

--- UdemyAsyncSocketServer/UdemyAsyncSocketServer/Form1.Designer.cs
+++ UdemyAsyncSocketServer/UdemyAsyncSocketServer/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.btnAcceptIncomingAsync = new System.Windows.Forms.Button();
+            this.btnSendAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnAcceptIncomingAsync
             // 
-            this.btnAcceptIncomingAsync.Location = new System.Drawing.Point(12, 216);
+            this.btnAcceptIncomingAsync.Location = new System.Drawing.Point(12, 327);
             this.btnAcceptIncomingAsync.Name = "btnAcceptIncomingAsync";
             this.btnAcceptIncomingAsync.Size = new System.Drawing.Size(178, 34);
             this.btnAcceptIncomingAsync.TabIndex = 0;
@@ -41,21 +44,55 @@
             this.btnAcceptIncomingAsync.UseVisualStyleBackColor = true;
             this.btnAcceptIncomingAsync.Click += new System.EventHandler(this.btnAcceptIncomingAsync_Click);
             // 
+            // btnSendAll
+            // 
+            this.btnSendAll.Location = new System.Drawing.Point(196, 327);
+            this.btnSendAll.Name = "btnSendAll";
+            this.btnSendAll.Size = new System.Drawing.Size(75, 34);
+            this.btnSendAll.TabIndex = 1;
+            this.btnSendAll.Text = "&Send All";
+            this.btnSendAll.UseVisualStyleBackColor = true;
+            this.btnSendAll.Click += new System.EventHandler(this.btnSendAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message: ";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(339, 327);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(361, 20);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.Text = "Message for client :) ;) :p";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(712, 373);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendAll);
             this.Controls.Add(this.btnAcceptIncomingAsync);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnAcceptIncomingAsync;
+        private System.Windows.Forms.Button btnSendAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMessage;
     }
 }
 
--- UdemyAsyncSocketServer/UdemyAsyncSocketServer/Form1.cs
+++ UdemyAsyncSocketServer/UdemyAsyncSocketServer/Form1.cs
@@ -25,5 +25,10 @@ namespace UdemyAsyncSocketServer
         {
             mServer.StartListeningForIncomingConnection();
         }
+
+        private void btnSendAll_Click(object sender, EventArgs e)
+        {
+            mServer.SendToAll(txtMessage.Text.Trim());
+        }
     }
 }
