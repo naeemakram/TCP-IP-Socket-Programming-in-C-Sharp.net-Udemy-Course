b3e391ad53400254e1a5558eae62b2e72742b612
Naeem Akram Malik <naeem.akram.malik@gmail.com>
2017-06-18 01:29:32 +0500

    27-create console application add reference and demo

--- UdemyAsyncSocketClient/UdemyAsyncSocketClient/Program.cs
+++ UdemyAsyncSocketClient/UdemyAsyncSocketClient/Program.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using LahoreSocketAsync;
 
 namespace UdemyAsyncSocketClient
 {
     class Program
     {
         static void Main(string[] args)
-        {          
+        {
+            LahoreSocketClient client = new LahoreSocketClient();
+
+            Console.WriteLine("*** Welcome to Socket Client Starter Example by Naeem Akram Malik ***");
+            Console.WriteLine("Please Type a Valid Server IP Address and Press Enter: ");
+
+            string strIPAddress = Console.ReadLine();
+
+            Console.WriteLine("Please Supply a Valid Port Number 0 - 65535 and Press Enter: ");
+            string strPortInput = Console.ReadLine();
+
+            if( !client.SetServerIPAddress(strIPAddress) || 
+                    !client.SetPortNumber(strPortInput))
+            {
+                Console.WriteLine(
+                    string.Format(
+                        "Wrong IP Address or port number supplied - {0} - {1} - Press a key to exit",
+                        strIPAddress, 
+                    strPortInput));
+                Console.ReadKey();
+                return;
+            }
+
+            client.ConnectToServer();
+
+            string strInputUser = null;
+
+            do
+            {
+                strInputUser = Console.ReadLine();
+            } while (strInputUser != "<EXIT>");
+
+
+
         }
     }
 }
